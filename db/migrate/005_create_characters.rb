62269fa8ca70b4c237ea1ed1fad5f8255dd9665e
github username <github email address>
2019-06-17 15:21:58 +0100

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,12 @@
+class CreateCharacters < ActiveRecord::Migration[4.2]
+    #to make an additive change to a schema we create
+    #a new migration, and then in the change method,
+    #we make the change.
+    def change
+      create_table :characters do |t|
+        t.string :name
+        t.integer :show_id
+    end
+  end
+end
+  
---
+++ db/migrate/006_add_actor_id_to_characters.rb
@@ -0,0 +1,6 @@
+class AddActorIdToCharacters < ActiveRecord::Migration[5.1]
+    def change
+      add_column :characters, :actor_id, :integer
+    #   add_column :characters, :catchphrase, :string
+    end
+  end
