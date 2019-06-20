8e3d61a24a40e25311b2c53d8ffd45dbee0c18b8
Le H Ngo <seiji@o2.pl>
2019-06-20 19:43:24 +0100

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+    def build_network(call_letters:)
+      self.network = Network.create(call_letters: call_letters)
+    end
 end
